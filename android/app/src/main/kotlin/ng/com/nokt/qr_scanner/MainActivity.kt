12dd3073f136a729c8e441368a5ad61b72424267
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-03 12:53:03 +0100

    Setup MobileScanner

---
+++ android/app/src/main/kotlin/ng/com/nokt/qr_scanner/MainActivity.kt
@@ -0,0 +1,6 @@
+package ng.com.nokt.qr_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Scanner App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  final MobileScannerController _scannerController = MobileScannerController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scanner"),
+        actions: [
+          IconButton(
+              onPressed: (){
+            _scannerController.switchCamera();
+          },
+              icon: const Icon(Icons.flip_camera_android)
+          )
+        ],
+      ),
+      body: MobileScanner(
+          allowDuplicates: false,
+          controller: _scannerController,
+          onDetect: (barcode, args) {
+            if (barcode.rawValue == null) {
+              debugPrint('Failed to scan Barcode');
+            } else {
+              final String code = barcode.rawValue!;
+              debugPrint('Barcode found! $code');
+            }
+          }),
+    );
+  }
+}
